70b0805b4fed38cd89f61d38d1c67cd3954382ec
Ualison Aguiar <ualison.aguiar@gmail.com>
2013-11-08 18:31:27 -0200

    Criação da nova funcionaliade de visualização de relatório por gráfico

---
+++ application/modules/default/controllers/RelatorioController.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Ualison Aguiar
+ *
+ * @copyright Ualison Aguiar
+ */
+
+/**
+ * Classe responsável pelos relatórios
+ *
+ * @author Ualison Aguiar <ualison.aguiar@gmail.com>
+ * @category Classe de teste
+ * @since 1.0
+ * @date 26/02/2012
+ */
+class Default_RelatorioController extends Default_SegurancaController
+{
+    public function graficoAction()
+    {
+        //http://www.jqplot.com/tests/cursor-highlighter.php
+    }
+
+    public function pesquisarResultadoGraficoAction()
+    {
+        exit('sasa');
+    }
+}
